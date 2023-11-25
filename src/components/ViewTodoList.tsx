81132f79bab7a3ae00c2b81ccb277838f32ec686
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-11-24 19:39:41 -0500

    Get getTodoParentsList

--- src/components/ViewTodoList.tsx
+++ src/components/ViewTodoList.tsx
@@ -25,6 +25,60 @@ const ViewTodoList: React.FC = () => {
     fetchTodos();
   }, []);
 
+  //todos is accessible here
+
+
+
+  const getTodoNameById = (id: number): string => {
+    const todo = todos.find(todo => todo.id === id);
+    return todo ? todo.name : 'Todo not found';
+  };
+
+  const getParentIdById = (id: number): number | undefined => {
+    const todo = todos.find(todo => todo.id === id);
+    return todo ? todo.parent_id : undefined;
+  };
+  
+  const getTodoById = (id: number): TodoType | undefined => {
+    return todos.find(todo => todo.id === id);
+  };
+
+  const getTodoParentsList = (initialTodo: TodoType) => {
+    let currentTodo: TodoType | undefined = initialTodo;
+    let result: number[] = [];
+  
+    while (currentTodo) {
+      result.push(currentTodo.id);
+      const parentId = getParentIdById(currentTodo.id);
+  
+      if (parentId) {
+        // Convert parentId to a number if it's a string
+        const numericParentId = typeof parentId === 'string' ? parseInt(parentId, 10) : parentId;
+        currentTodo = todos.find(todo => todo.id === numericParentId);
+      } else {
+        currentTodo = undefined; // Stop the loop if there is no parent
+      }
+    }
+  
+    return result;
+  };
+  
+
+
+  const foo = () => {
+    const todoId = 88; // Replace 1 with the actual ID you want to use
+    const initTodo = getTodoById(todoId);
+  
+    if (initTodo) {
+      console.log(getTodoParentsList(initTodo));
+      console.log(initTodo);
+      console.log("underwareter")
+    } else {
+      console.log('Todo not found');
+    }
+  };
+  
+
   return (
     <div>
       <h1>Todo List</h1>
@@ -48,6 +102,7 @@ const ViewTodoList: React.FC = () => {
           ))}
         </tbody>
       </table>
+      <button onClick={foo}>Step 2 </button>
     </div>
   );
 };
