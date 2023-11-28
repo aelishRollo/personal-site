26ba53057b54d4402b93ae60e9a3cc5474307d70
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-11-28 16:22:21 -0500

    Prepare to hoist isVisible to Todo.tsx

--- src/components/RenderSpecificTodo.tsx
+++ src/components/RenderSpecificTodo.tsx
@@ -1,10 +1,21 @@
+
+    //I like the convention of having comments at the top. And of removing comments as much as possible.
+    //Keep it tidy.
+
+    //I want the code in this file to largely be the same. So I want it to get isVisisible
+    //and its setter as props from Todo.tsx
+
+    //To do that I'll build a second button which is basically the same, just with an arbitrary name, get that functioning properly,
+    //then copy and paste its code back to the first button. Like building a clone of it to make sure I can copy it. 
+
+
 import { useState } from "react"
 export default function RenderSpecifcTodo() {
     let result = <> </>
     let [isVisible,setIsVisible] = useState(false)
 
-    const handleButtonClick = () => {
-        setIsVisible(!isVisible)
+    const handleButtonClick = () => {   
+        setIsVisible(!isVisible)    //change isVisible's value
     }
 
     if (isVisible) {
@@ -13,7 +24,6 @@ export default function RenderSpecifcTodo() {
     if (!isVisible) {
         result = <> </>
     }
-
 
     return (
       <>
