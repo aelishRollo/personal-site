691efeed08ead3231ce19e9b672d1ec0b6eaed91
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-12-03 13:26:55 -0500

    Add button that logs parents to the console

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -102,10 +102,65 @@ export default function App() {
         )
     }
 
+    const getTodoNameById = (id: number): string => {
+        const todo = todos.find(todo => todo.id === id);
+        return todo ? todo.name : 'Todo not found';
+      };
+    
+      const getParentIdById = (id: number): number | undefined => {
+        const todo = todos.find(todo => todo.id === id);
+        return todo ? todo.parent_id : undefined;
+      };
+      
+      const getTodoById = (id: number): TodoType | undefined => {
+        return todos.find(todo => todo.id === id);
+      };
+      
+    
+      const getTodoParentsList = (initialTodo: TodoType) => {
+        let currentTodo: TodoType | undefined = initialTodo;
+        let result: number[] = [];
+      
+        while (currentTodo) {
+          result.push(currentTodo.id);
+          const parentId = getParentIdById(currentTodo.id);
+      
+          if (parentId) {
+            // Convert parentId to a number if it's a string
+            const numericParentId = typeof parentId === 'string' ? parseInt(parentId, 10) : parentId;
+            currentTodo = todos.find(todo => todo.id === numericParentId);
+          } else {
+            currentTodo = undefined; // Stop the loop if there is no parent
+          }
+        }
+      
+        return result;
+      };
+      
+    
+    
+      const getTodoParentsStringList = (todoId:number) => {
+        const initTodo = getTodoById(todoId);
+        let result:string[] = []
+      
+        if (initTodo) {
+          let a = getTodoParentsList(initTodo)
+          
+      
+          for (let i = 0; i < a.length; i++) {
+            result.push(getTodoNameById(a[i]))
+          };
+        } else {
+          console.log('Todo not found');
+        }
+        console.log(result)
+      };
+
 
     return (
         <div className="App-header">
             <IsVisibleController />
+            <button onClick={() => {getTodoParentsStringList(88)}}> Lomo</button>
         </div>
     );
 }
