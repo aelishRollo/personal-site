1a348486a4531e8f913402f9d556fa4fd20b7baf
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-12-09 13:19:10 -0500

    Add to AboutMusic.tsx

--- src/components/AboutMusic.tsx
+++ src/components/AboutMusic.tsx
@@ -11,7 +11,23 @@ export default function Contact() {
 
                 <h4> Songwriting and Improvisation:</h4>
                 <p className='App-paragraph'>
-                    What
+                    I've always been a lover and close listener of music. Like mathematics and programming, it is another
+                    form of language. Of expression. The things that can be expressed through music are so nuanced and specific,
+                    and I find that irresistable. I think there are human experiences we're all familar with that can only be expressed
+                    with music. For example, why does <a href='https://www.youtube.com/watch?v=fCV9oqtwyVA' target="_blank">Yesterday</a> by the Beatles pull at your heartstrings the way that it does? To be able 
+                    to evoke that kind of emotion, that's one of the core things that draws me to music, and to songwriting.
+                </p>
+                <p className='App-paragraph'>
+                    To this end, I write my own songs. I find the act of songwriting to be enthralling, of the utmost importance,
+                    infinitely complicated, and frequently frustrating. It's no easy thing to write a timeless song that hits the 
+                    soul in that way, but I want to so bad. Even if I don't know how, I'm going to keep trying.
+                </p>
+                <p className='App-paragraph'>
+                    I also love to improvise. If I have a musical superpower, it's that I can improvise on pretty much any instrument.
+                    Especially if there is already music playing, I can play along to it, inventing my own lines and phrases that mesh
+                    with what's already there, adding to it without distracting from it. I love to jam with other musicians. My absolute 
+                    favorite moments are the flow states, when playing becomes totally effortless. It's like all the musicians are a hivemind,
+                    totally synchronized. It's almost like a form of hypnosis, and the subconscious takes over.
                 </p>
 
                 <h4> Instruments:</h4>
@@ -32,7 +48,10 @@ export default function Contact() {
                     why it has such a special place in my heart.
                 </p>
                 <p className='App-paragraph'>
-                    I grew up singing with my family.
+                    I grew up singing with my family. I was in a choir as a child, doing Christmas performances. As a teenager
+                    I spent several years participating in musical theater which was thoroughly enjoyable. It taught me a lot
+                    about singing, performing, people, among other things. To this day, singing feelings natural, just like an
+                    extension of talking.
                 </p>
             </header >
         </div>
