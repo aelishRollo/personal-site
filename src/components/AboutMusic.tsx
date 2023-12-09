c7feb8f4686cde5ccbcc028ae786c5572da4e601
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-12-09 11:11:38 -0500

    Add HomeButton.tsx to AboutMusic.tsx

--- src/components/AboutMusic.tsx
+++ src/components/AboutMusic.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import '../App.css';
-import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import HomeButton from './HomeButton';
+
 
 export default function Contact() {
 
     return (
         <div className="App">
             <header className="App-header">
+            <HomeButton />
                 <p>
                     I sing, play guitar, keys and drums. I also mix my own music. I'm a nerd for music theory,
                     but above all else, I love to improvise.
