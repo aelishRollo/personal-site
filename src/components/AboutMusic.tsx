5e2cebfa20154cd40853d10f252ab85830718947
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-12-09 13:02:02 -0500

    Update AboutMusic.tsx

--- src/components/AboutMusic.tsx
+++ src/components/AboutMusic.tsx
@@ -7,10 +7,32 @@ export default function Contact() {
     return (
         <div className="App">
             <header className="App-header">
-            <HomeButton />
-                <p>
-                    I sing, play guitar, keys and drums. I also mix my own music. I'm a nerd for music theory,
-                    but above all else, I love to improvise.
+                <HomeButton />
+
+                <h4> Songwriting and Improvisation:</h4>
+                <p className='App-paragraph'>
+                    What
+                </p>
+
+                <h4> Instruments:</h4>
+                <p className='App-paragraph'>
+                    There's always been a piano in my home, so that's where I'm most comfortable. I love acoustic pianos,
+                    since they're so familar to me. I love the way all the notes are laid out, as it allows for me to visually
+                    see the relationships between the scales and their notes. This is immensely helpful for both improvisation
+                    and composition
+                </p>
+                <p className='App-paragraph'>
+                    I also love electronic keyboards, largely because they're so versatile. With just one device and a DAW, I can
+                    play thousands of instruments. Thousands of textures. It's staggeringly powerful.
+                </p>
+                <p className='App-paragraph'>
+                    I absolutely love the sound of guitar, both acoustic and electric. I find that as instruments go, this is
+                    the one I'm most drawn to. There's just something emotionally evocative about it. Everything from folk music,
+                    classic rock, psychedelic rock, indie rock, alternative rock, it's all so dependent on the guitar. That's part of
+                    why it has such a special place in my heart.
+                </p>
+                <p className='App-paragraph'>
+                    I grew up singing with my family.
                 </p>
             </header >
         </div>
