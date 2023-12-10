3e987efdfc98f47b85067e97b2e053254ac60b61
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2023-12-09 19:17:53 -0500

    Update AboutDev.tsx

--- src/components/AboutDev.tsx
+++ src/components/AboutDev.tsx
@@ -7,12 +7,24 @@ export default function Contact() {
         <div className="App">
             <header className="App-header">
                 <HomeButton />
-                <p>
-                    I'm currently using node.js with typescript and React. I'm in favor of test driven development.
-                    Also, I think it's beautiful that with some basic programming knowledge, we have the power to create new tools for anything we
-                    dream up. 
+                <h4>My Current Technologies</h4>
+                <p className='App-paragraph'>
+                    Right now I'm mainly using node.js with typescript and React. I've been using Supabase as a backend and having a good
+                    expereince with that.
                 </p>
-        </header >
+                <h4>Personal Philosophies</h4>
+                <p className='App-paragraph'>
+                    I'm in favor of <a href='https://en.wikipedia.org/wiki/Test-driven_development' target='_blank'> test driven development</a>,
+                    as well as Typescript over vanilla Javascript because in any
+                    serious application I think the more maintainible, robust, and mature the codebase is, the better.
+
+                </p>
+                <p className='App-paragraph'>
+                    Also, I think it's beautiful that with some knowledge and hard work, we have the power to create new tools and technologies for
+                    anything we can think of. Unlike creators of most technolgies, our work does not require physical materials. It's honestly
+                    quite beautiful.
+                </p>
+            </header >
         </div >
 
     );
