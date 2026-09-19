54598a0352cc2e18dc18db66e69f7d3e3f259026
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-09-19 19:44:55 -0400

    Add Liquid Dream skin to bootstrap registry

--- assets/js/skin-bootstrap.js
+++ assets/js/skin-bootstrap.js
@@ -17,7 +17,8 @@
 		{ id: 'portfolio-light', name: 'Portfolio Light', scheme: 'light', css: '' },
 		{ id: 'acid-editorial', name: 'Acid Editorial', scheme: 'light', css: 'assets/css/skins/acid-editorial.css' },
 		{ id: 'op-art-monochrome', name: 'Op Art Monochrome', scheme: 'light', css: 'assets/css/skins/op-art-monochrome.css' },
-		{ id: 'neon-glitch', name: 'Neon Glitch', scheme: 'dark', css: 'assets/css/skins/neon-glitch.css' }
+		{ id: 'neon-glitch', name: 'Neon Glitch', scheme: 'dark', css: 'assets/css/skins/neon-glitch.css' },
+		{ id: 'liquid-dream', name: 'Liquid Dream', scheme: 'light', css: 'assets/css/skins/liquid-dream.css' }
 	];
 	var skinById = {};
 	var activeId = '';
