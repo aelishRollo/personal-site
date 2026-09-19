7c96fe8aa3c41bec5b201fc32295930a18cd96a2
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-09-19 19:40:17 -0400

    Add Neon Glitch skin to bootstrap configuration

--- assets/js/skin-bootstrap.js
+++ assets/js/skin-bootstrap.js
@@ -16,7 +16,8 @@
 		{ id: 'portfolio-dark', name: 'Portfolio Dark', scheme: 'dark', css: '' },
 		{ id: 'portfolio-light', name: 'Portfolio Light', scheme: 'light', css: '' },
 		{ id: 'acid-editorial', name: 'Acid Editorial', scheme: 'light', css: 'assets/css/skins/acid-editorial.css' },
-		{ id: 'op-art-monochrome', name: 'Op Art Monochrome', scheme: 'light', css: 'assets/css/skins/op-art-monochrome.css' }
+		{ id: 'op-art-monochrome', name: 'Op Art Monochrome', scheme: 'light', css: 'assets/css/skins/op-art-monochrome.css' },
+		{ id: 'neon-glitch', name: 'Neon Glitch', scheme: 'dark', css: 'assets/css/skins/neon-glitch.css' }
 	];
 	var skinById = {};
 	var activeId = '';
